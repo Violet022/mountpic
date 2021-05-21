dbed557be0e04f9c5029475ba5ac406499d1578d
anna <annatorgun@mail.ru>
2021-05-21 17:22:03 +0700

    scaling alg

--- app/src/main/java/com/example/mountpic/ScalingFragment.kt
+++ app/src/main/java/com/example/mountpic/ScalingFragment.kt
@@ -1,74 +1,76 @@
 package com.example.mountpic
 
-import android.graphics.drawable.BitmapDrawable
+import android.graphics.Bitmap
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
 import androidx.fragment.app.Fragment
-import kotlin.math.floor
 
 
-class ScalingFragment : Fragment(R.layout.fragment_scaling){
+class ScalingFragment : Fragment(R.layout.fragment_scaling) {
 
     private lateinit var photoIm: ImageView
-    private lateinit var buttonScal: Button
+    private lateinit var buttonScala: Button
+    private lateinit var scalingFactor: EditText
 
-    companion object{
+    companion object {
         val TAG = ScalingFragment::class.java.simpleName
         fun newInstance() = ScalingFragment()
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val rootView = inflater.inflate(R.layout.fragment_rotation, container, false)
+        val rootView = inflater.inflate(R.layout.fragment_scaling, container, false)
 
         photoIm = rootView.findViewById(R.id.photoToScaling)
-        buttonScal = rootView.findViewById(R.id.buttonScaling)
+        buttonScala = rootView.findViewById(R.id.buttonScaling)
+        scalingFactor = rootView.findViewById(R.id.factor)
 
-        var photo = (context as SecondPage).fromUriToBitmap()
-        photoIm.setImageBitmap(photo)
+        var image = (context as SecondPageActivity).fromUriToBitmap()
+        photoIm.setImageBitmap(image)
 
-        buttonScal.setOnClickListener() {
-
+        buttonScala.setOnClickListener() {
+            lateinit var scalingImage: Bitmap
+            val kFactor: Double = (scalingFactor.text.toString()).toDouble()
+            scalingImage = resizePixels(image, kFactor)
+            image = scalingImage
+            photoIm.setImageBitmap(scalingImage)
         }
+
         return rootView
     }
 
-    /*override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?{
-        val view: View = inflater.inflate(R.layout.fragment_scaling, container, false)
-        val imagePic: ImageView = view.findViewById(R.id.image_view) as ImageView
+    fun resizePixels(data: Bitmap, factor: Double): Bitmap {
 
-        val image = (imagePic.drawable as BitmapDrawable).bitmap
-        val imageWidth = image.width
-        val imageHeight = image.height
-        val imagePixels = IntArray(imageWidth * imageHeight)
-        image.getPixels(imagePixels, 0, imageWidth, 0, 0, imageWidth, imageHeight)
+        val newWidth = (data.width * factor).toInt()
+        val newHeight = (data.height * factor).toInt()
+        val bitmapConvert = Bitmap.createBitmap(newWidth, newHeight, data.config)
 
-        return view
-    }
-}*/
-
-///алгоритхм
-fun resizePixels(pixels: IntArray, ratio: Int): IntArray? {
-    val temp = IntArray(imageWidth * imageHeight * ratio)
-    var px: Double
-    var py: Double
-    for (i in 0 until imageHeight * ratio) {
-        for (j in 0 until imageWidth * ratio) {
-            px = floor((j * ratio).toDouble())
-            py = floor((i * ratio).toDouble())
-            temp[i * imageWidth * ratio + j] = pixels[(py * imageWidth + px).toInt()]
+        for(i in 0 until bitmapConvert.width){
+            for(j in 0 until bitmapConvert.height){
+                bitmapConvert.setPixel(i, j, Color.argb(Color.alpha(0), 0,0,0))
+            }
         }
+
+        for(i in 0 until bitmapConvert.width){
+            for(j in 0 until bitmapConvert.height){
+                val scrx = (i/factor).toInt()
+                val scry = (j/factor).toInt()
+                val p = data.getPixel(scrx, scry)
+                val r = Color.red(p)
+                val g = Color.green(p)
+                val b = Color.blue(p)
+
+                bitmapConvert.setPixel(i, j, Color.argb(Color.alpha(p), r, g, b))
+            }
+        }
+
+        return bitmapConvert
     }
-    return temp
 }
 
-
-//план
-//1.получить битмап
-//2.массив пикселей
-//3.ролик коэффициентов со считыванием значения
-//4.кнопочка
-//5.алгоритм ближайшего соседа
+//5.бонусный варик
